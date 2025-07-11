c28475193208825de72a89e1efdeaf243d9de3be
thehxdev <hossein.khosravi.ce@gmail.com>
2025-07-11 18:27:00 +0330

    handle multiple files - bump to version 0.3.0
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- main.c
+++ main.c
@@ -6,56 +6,60 @@
 
 // include newline and null terminator
 #define BUFFER_SIZE (2048+2)
-#define VERSION_STRING "0.2.0"
-
-#define file_close(f) \
-    if ((f)) \
-        fclose((f))
+#define VERSION_STRING "0.3.0"
 
 typedef struct bloom bloom_t;
+
+void handle_file(bloom_t *bloom, FILE *fp);
 
 int main(int argc, char *argv[]) {
     FILE *fp;
     int err = 0;
-    char buf[BUFFER_SIZE];
+    bloom_t bloom;
 
-    if (argc == 2) {
+    if ( (err = bloom_init2(&bloom, (1 << 20), 0.01)))
+        goto ret;
+
+    if (argc >= 2) {
         if (!strcmp(argv[1], "-h")) {
             fprintf(stderr,
                     "bloniq v"VERSION_STRING
                     "\nUsage: %s [input-file]\n",
                     argv[0]);
-            return 1;
-        } else {
-            if (!(fp = fopen(argv[1], "rb"))) {
+            goto ret_bloom_free;
+        }
+        argc--;
+        argv++;
+        while (argc--) {
+            fp = fopen(*argv++, "rb");
+            if (!fp) {
                 perror("fopen");
-                err = errno;
-                goto ret;
+                continue;
             }
+            handle_file(&bloom, fp);
+            fclose(fp);
         }
     } else {
         fp = stdin;
+        handle_file(&bloom, fp);
     }
 
-    bloom_t bloom;
-    if ( (err = bloom_init2(&bloom, (1 << 20), 0.01)))
-        goto ret_fp_close;
+ret_bloom_free:
+    bloom_free(&bloom);
+ret:
+    return err;
+}
 
+void handle_file(bloom_t *bloom, FILE *fp) {
     size_t buf_len;
+    char buf[BUFFER_SIZE];
     while (fgets(buf, BUFFER_SIZE, fp)) {
         buf_len = strlen(buf);
         // remove the newline
         buf[buf_len] = 0; buf_len--;
-        if (!bloom_check(&bloom, buf, buf_len)) {
-            bloom_add(&bloom, buf, buf_len);
+        if (!bloom_check(bloom, buf, buf_len)) {
+            bloom_add(bloom, buf, buf_len);
             fputs(buf, stdout);
         }
     }
-
-    bloom_free(&bloom);
-
-ret_fp_close:
-    file_close(fp);
-ret:
-    return err;
 }
